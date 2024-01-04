167eb216d4997c615fc66ddc3d64eb75df6e76d1
Tsung-Han Hsieh <64431591+Lucashien@users.noreply.github.com>
2024-01-04 17:35:54 +0800

    Delete some file

--- hw1/dlcv-fall-2023-hw1-Lucashien-main/hw1_1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO - run your inference Python3 code
-python3 P1_inference.py $1 $2
--- hw1/dlcv-fall-2023-hw1-Lucashien-main/hw1_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO - run your inference Python3 code
-python3 P2_inference.py $1 $2 $3
